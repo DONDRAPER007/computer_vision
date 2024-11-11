28568f764e6eccc6e6d562ec742273cd5a54135c
DONDRAPER007 <52046596+DONDRAPER007@users.noreply.github.com>
2024-11-11 20:51:22 +0300

    Update tracker.py

--- yolov10_detection_and_tracking/tracker.py
+++ yolov10_detection_and_tracking/tracker.py
@@ -4,10 +4,10 @@ from deep_sort_realtime.deepsort_tracker import DeepSort
 class Tracker:
   def __init__(self):
     self.object_tracker = DeepSort(
-        max_age=20,
+        max_age=4,
         n_init=2,
         nms_max_overlap=0.3,
-        max_cosine_distance=0.8,
+        max_cosine_distance=0.5,
         nn_budget=None,
         override_track_class=None,
         embedder="mobilenet",
